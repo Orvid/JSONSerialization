0f9cd928ac8be7af3b2536df7f447aa151fd8c02
Orvid King <blah38621@gmail.com>
2013-11-02 18:58:39 -0500

    Minor cleanup, and made the project runnable on windows.

--- JSONSerialization/main.d
+++ JSONSerialization/main.d
@@ -2,7 +2,6 @@ module main;
 
 import std.stdio;
 import std.datetime : StopWatch;
-import std.datetime : benchmark;
 import std.serialization : serializable;
 import std.json : toJSON, fromJSON;
 import std.conv : to;
@@ -10,51 +9,50 @@ import std.range : Appender, isOutputRange;
 
 enum ObjectCount = 100000;
 
-@trusted pure struct CustomStringRange
+struct CustomStringRange
 {
-@trusted:
-	private char[] mBuffer = ['\0'];
+	private char[] mBuffer;
 	private size_t nextI = 0;
 
-	private void ensureSpace(size_t len)
+	private void ensureSpace(size_t len) @safe pure nothrow
 	{
 		while (nextI + len >= mBuffer.length)
 			mBuffer.length = (mBuffer.length ? mBuffer.length : 1) << 1;
 	}
 
-	@property string data()
+	@property string data() @trusted pure
 	{
 		return cast(string)mBuffer[0..nextI].dup;
 	}
 
-	void clear()
+	void clear() @safe pure nothrow
 	{
 		nextI = 0;
 		mBuffer[] = 0;
 	}
 
-	void put(string str)
+	void put(string str) @safe pure nothrow
 	{
 		ensureSpace(str.length);
 		mBuffer[nextI..str.length + nextI] = str[0..$];
 		nextI += str.length;
 	}
 	
-	void put(char[] str)
+	void put(char[] str) @safe pure nothrow
 	{
 		ensureSpace(str.length);
 		mBuffer[nextI..str.length + nextI] = str[0..$];
 		nextI += str.length;
 	}
 	
-	void put(const(char)[] str)
+	void put(const(char)[] str) @safe pure nothrow
 	{
 		ensureSpace(str.length);
 		mBuffer[nextI..str.length + nextI] = str[0..$];
 		nextI += str.length;
 	}
 
-	void put(char c)
+	void put(char c) @safe pure nothrow
 	{
 		ensureSpace(1);
 		mBuffer[nextI] = c;
